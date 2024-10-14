4e938a46ae02e54b7882c6614c4ebe746141ef1a
scooby75 <45230721+scooby75@users.noreply.github.com>
2024-10-13 22:55:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ xg_mandante = st.number_input("Informe o xG do Mandante", min_value=0.0, value=1
 xg_visitante = st.number_input("Informe o xG do Visitante", min_value=0.0, value=1.2, step=0.1)
 
 # Botão para calcular a probabilidade ajustada, a odd e a linha de handicap asiático
-if st.button("Calcular Probabilidade, Odd e Linha de Handicap"):
+if st.button("Calcular"):
     probabilidade = calcular_probabilidade_ajustada(elo_mandante, elo_visitante, xg_mandante, xg_visitante) * 100
     odd = 100 / probabilidade if probabilidade > 0 else float('inf')  # Evitar divisão por zero
     linha_handicap = estimar_handicap_asiatico(probabilidade)
